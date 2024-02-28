f84a6b389e242b677e76c2e25b098623abac57c3
Nguyen Thanh Nhat <nguyennhat082004@gmail.com>
2024-02-28 21:25:49 +0700

    sumof2so

---
+++ BTH1/SumOf2Numbers.java
@@ -0,0 +1,16 @@
+package BTH1;
+public class SumOf2Numbers {
+       private int a,b;
+       SumOf2Numbers(int a,int b)
+       {
+        this.a = a;this.b = b;
+       }
+       public int sum()
+       {
+        return this.a + this.b;
+       }
+      public static void main(String[] args) {
+        SumOfTwoNumbers a = new SumOf2Numbers(5, 6);
+          System.out.println(a.sum());
+      }
+}

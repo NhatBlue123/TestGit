fd3f7d27c678ef6fb3ff421f3fe7ead221172564
Nguyen Thanh Nhat <nguyennhat082004@gmail.com>
2024-02-29 12:23:17 +0700

    adds

---
+++ BTH1/src/main/java/com/mycompany/bth12024/BTH12024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bth12024;
+
+/**
+ *
+ * @author bluez
+ */
+public class BTH12024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
